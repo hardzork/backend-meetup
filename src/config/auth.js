9d58b014e4df162588cbda36aca3b172f9fa15f3
Robinson Junior <hardzork@macjr.local>
2019-07-02 09:51:12 -0300

    Challenge Completed.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'c08cbbfd6eefc83ac6d23c4c791277e4',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,19 @@
 import { Router } from 'express';
 
-// import UserController from './app/controllers/UserController';
-// import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-// import authMiddleware from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+/**
+ * Users
+ */
+routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+/**
+ * Session
+ */
+routes.post('/sessions', SessionController.store);
 
-// routes.post("/users", UserController.store);
-// routes.put("/users", authMiddleware, UserController.update);
-
-// routes.post("/sessions", SessionController.store);
-
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Diego Fernandes',
-//     email: 'diego@rocketseat.com.br',
-//     password_hash: '123',
-//   });
-//   return res.json(user);
-// });
 export default routes;
