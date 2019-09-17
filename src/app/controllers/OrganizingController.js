e4aaf64f8374c989df117b82b455746b9a672d12
Robinson Junior <robinson.juniorr@gmail.com>
2019-09-17 18:02:31 -0300

    change response of the organizing to group by date

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,10 +1,21 @@
+import { parseISO, startOfDay, isEqual } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class OrganizingController {
   async index(req, res) {
     const meetups = await Meetup.findAll({ where: { user_id: req.userID } });
-
-    return res.json(meetups);
+    const distinctDates = [
+      ...new Set(meetups.map(meetup => startOfDay(meetup.date).toISOString())),
+    ];
+    const data = distinctDates.map(dt => {
+      return {
+        date: parseISO(dt),
+        meetups: meetups.filter(meetup =>
+          isEqual(parseISO(dt), startOfDay(meetup.date))
+        ),
+      };
+    });
+    return res.json(data);
   }
 }
 
