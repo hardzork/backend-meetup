df5abd46b543d618e80a4a54e2feebe5b7fb26ad
Robinson Junior <robinson.juniorr@gmail.com>
2019-10-30 15:28:37 -0300

    add user filter on search meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -58,7 +58,7 @@ class MeetupController {
       description: Yup.string().required(),
       location: Yup.string().required(),
       date: Yup.date().required(),
-      file_id: Yup.number().required(),
+      // file_id: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,32 +1,9 @@
-import { parseISO, startOfDay, endOfDay, isEqual } from 'date-fns';
-import { Op } from 'sequelize';
 import Meetup from '../models/Meetup';
 
 class OrganizingController {
   async index(req, res) {
-    const { startDate, endDate } = req.query;
-    const parsedStartDate = parseISO(startDate);
-    const parsedEndDate = parseISO(endDate);
-    const meetups = await Meetup.findAll({
-      where: {
-        user_id: req.userID,
-        date: {
-          [Op.between]: [startOfDay(parsedStartDate), endOfDay(parsedEndDate)],
-        },
-      },
-    });
-    const distinctDates = [
-      ...new Set(meetups.map(meetup => startOfDay(meetup.date).toISOString())),
-    ];
-    const data = distinctDates.map(dt => {
-      return {
-        date: parseISO(dt),
-        meetups: meetups.filter(meetup =>
-          isEqual(parseISO(dt), startOfDay(meetup.date))
-        ),
-      };
-    });
-    return res.json(data);
+    const meetups = await Meetup.findAll({ where: { user_id: req.userID } });
+    return res.json(meetups);
   }
 }
 
